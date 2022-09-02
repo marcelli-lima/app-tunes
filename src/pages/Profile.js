5dc4fea08e43a3dcab1cdc94667d83f44f29a563
marcelli-lima <marcelli_lima@hotmail.com>
2022-09-02 15:40:31 -0300

    requisito bonus 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      user: {},
+    };
+  }
+
+  async componentDidMount() {
+    const userLogado = await getUser();
+    this.setState({ user: userLogado, loading: false });
+  }
+
   render() {
+    const { user, loading } = this.state;
+    console.log(user);
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        { loading
+          ? <Loading />
+          : (
+            <div>
+              <img data-testid="profile-image" alt={ user.name } src={ user.image } />
+              <h3>Nome:</h3>
+              <p>{ user.name }</p>
+              <h3>Email:</h3>
+              <p>{ user.email }</p>
+              <h3>Descrição:</h3>
+              <p>{ user.description }</p>
+              <Link to="/profile/edit">Editar perfil</Link>
+            </div>)}
       </div>
     );
   }
